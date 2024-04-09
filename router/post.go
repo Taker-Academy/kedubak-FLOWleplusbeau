e49727ee4b8bb01c7016aacaf8c77dbd0fc1babd
Florian <f0208r@gmail.com>
2024-04-09 19:44:57 +0200

    [ADD] delete post

--- router/post.go
+++ router/post.go
@@ -20,6 +20,7 @@ func PostRoutes(app *fiber.App, db *mongo.Database) {
 	GetMyPosts(db, post)
 	GetPostById(db, post)
 	CreatePost(db, post)
+	DeletePostById(db, post)
 }
 
 func CreatePost(db *mongo.Database, post fiber.Router) {
@@ -216,6 +217,65 @@ func GetPostById(db *mongo.Database, post fiber.Router) {
 				"content":   post.Content,
 				"comments":  post.Comments,
 				"upVotes":   post.UpVotes,
+			},
+		})
+	})
+}
+
+func DeletePostById(db *mongo.Database, post fiber.Router) {
+	post.Delete("/:id", func(c *fiber.Ctx) error {
+		// get user id and authorization from token
+		UserId, err := jwt.GetUserID(c.Get("Authorization"), db.Client())
+		if err != nil {
+			return c.Status(http.StatusUnauthorized).JSON(fiber.Map{
+				"ok":    false,
+				"error": "wrong token",
+			})
+		}
+
+		// get post by id
+		postCollection := db.Collection("Post")
+		postId := c.Params("id")
+		objId, _ := primitive.ObjectIDFromHex(postId)
+		post := models.Post{}
+
+		// get post from db
+		err = postCollection.FindOne(context.Background(), bson.M{"_id": objId}).Decode(&post)
+		if err != nil {
+			return c.Status(http.StatusNotFound).JSON(fiber.Map{
+				"ok":    false,
+				"error": "Post not found",
+			})
+		}
+
+		// check if the post belongs to the user
+		if post.UserId != UserId {
+			return c.Status(http.StatusForbidden).JSON(fiber.Map{
+				"ok":    false,
+				"error": "Forbidden",
+			})
+		}
+
+		// delete post
+		_, err = postCollection.DeleteOne(context.Background(), bson.M{"_id": objId})
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"ok":    false,
+				"error": "Internal Server Error",
+			})
+		}
+
+		return c.Status(http.StatusOK).JSON(fiber.Map{
+			"ok": true,
+			"message": fiber.Map{
+				"createdAt": post.CreatedAt,
+				"userId":    post.UserId,
+				"firstName": post.FirstName,
+				"title":     post.Title,
+				"content":   post.Content,
+				"comments":  post.Comments,
+				"upVotes":   post.UpVotes,
+				"removed":   true,
 			},
 		})
 	})
